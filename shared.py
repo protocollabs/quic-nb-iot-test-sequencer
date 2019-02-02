55e64899fc5fea905e7169786f9bd771d42b9b13
Hagen Paul Pfeifer <hagen@jauu.net>
2019-02-02 16:18:21 +0100

    ctx: shared function requires globale knowledge too
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- shared.py
+++ shared.py
@@ -1,15 +1,15 @@
-def prepare_mapago():
+def prepare_mapago(ctx):
 	print("Preparing mapago")
 
-def prepare_netem():
+def prepare_netem(ctx):
 	print("Preparing netem")
 
-def prepare_server():
+def prepare_server(ctx):
 	print("Preparing server")
 
-def prepare_client():
+def prepare_client(ctx):
 	print("Preparing client")
 
-def analyze_data():
+def analyze_data(ctx):
 	print("Calling matplotlib to analyze msmt results")
 
