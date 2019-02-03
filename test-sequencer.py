971c00df3ecc8ffe15cb31c78405df05384e6925
Hagen Paul Pfeifer <hagen@jauu.net>
2019-02-03 11:19:44 +0100

    ssh keys: allow ~ in path
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- test-sequencer.py
+++ test-sequencer.py
@@ -13,12 +13,19 @@ CONFIG_NAME = 'network.conf'
 def update_software(ctx):
     print("Dummy Func to update software on Mapago topology")
 
+def config_subst_path(config):
+    """ all path with ~ are substitured with the full path"""
+    if 'ssh' in config and 'keyfilepath' in config['ssh']:
+        path = os.path.expanduser(config['ssh']['keyfilepath'])
+        config['ssh']['keyfilepath'] = path
+
 def config_load():
     root_dir = os.path.dirname(os.path.realpath(__file__))
     config_path = os.path.join(root_dir, CONFIG_NAME)
     config = dict()
     exec(open(config_path).read(), config)
     config.pop("__builtins__")
+    config_subst_path(config)
     return config
 
 def context_init():
