c7947d0c37aa6eab398a6d584f95083668764fd6
monfron <simon.fronhoefer@gmail.com>
2019-02-05 15:14:45 +0100

    Added: Hand over config via parameter
    Signed-off-by: monfron <simon.fronhoefer@gmail.com>

--- test-sequencer.py
+++ test-sequencer.py
@@ -3,6 +3,7 @@
 import os
 import types
 import json
+import argparse
 
 from tests import throughput_max
 
@@ -12,11 +13,13 @@ CONFIG_NAME = 'network.conf'
 def update_software(ctx):
     print("Dummy Func to update software on Mapago topology")
 
+
 def config_subst_path(config):
     """ all path with ~ are substitured with the full path"""
     if 'ssh' in config and 'keyfilepath' in config['ssh']:
         path = os.path.expanduser(config['ssh']['keyfilepath'])
         config['ssh']['keyfilepath'] = path
+
 
 def config_load():
     root_dir = os.path.dirname(os.path.realpath(__file__))
@@ -27,16 +30,46 @@ def config_load():
     config_subst_path(config)
     return config
 
-def context_init():
+
+def custom_config_load(custom_conf):
+    if os.path.isabs(custom_conf) is not True:
+        raise Exception('\nabs path needed! absolute path starts with /')
+
+    config = dict()
+    exec(open(custom_conf).read(), config)
+    config.pop("__builtins__")
+    config_subst_path(config)
+    return config
+
+
+def context_init(custom_conf):
     ctx = types.SimpleNamespace()
-    ctx.config = config_load()
+
+    if custom_conf is not None:
+        ctx.config = custom_config_load(custom_conf)
+    else:
+        ctx.config = config_load()
+
     return ctx
 
-def main():
-    ctx = context_init()
-    update_software(ctx)
-    throughput_max.main(ctx)
 
+def main():
+    # parse args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", help="append path to your own config")
+    args = parser.parse_args()
+
+    if args.config:
+        ctx = context_init(args.config)
+    else:
+        ctx = context_init(None)
+
+    # TODO: This should call go get github.com/.../mapago => improvement
+    update_software(ctx)
+
+    print("\nreturned conf {}".format(ctx))
+
+    throughput_max.main(ctx)
 
 if __name__ == '__main__':
     main()
