216937bfc544a864e019e675bd041d97c20f81d7
monfron <simon.fronhoefer@gmail.com>
2019-02-05 15:15:43 +0100

    Fixed: ssh_execute now running, Formatting
    fixed some bugs:
    	- hand over of NONE as private key
    	- no type casting
    	- handover password if private key is encrypted
    
    ongoing bugs:
    	- we cannot sign in via private key => temporarily we use username/password tuple
    	- the interfaces in netem are not recognized
    
    Signed-off-by: monfron <simon.fronhoefer@gmail.com>

--- shared.py
+++ shared.py
@@ -1,8 +1,12 @@
 import os
 import paramiko
+import time
+
+yourPw = "yourpasswd"
 
 def prepare_mapago(ctx):
-	print("Preparing mapago")
+    print("Preparing mapago")
+
 
 def netem_reset(ctx, hostname, interfaces):
     print('reset interfaces: {}'.format(interfaces))
@@ -12,33 +16,40 @@ def netem_reset(ctx, hostname, interfaces):
         print(out)
         print(err)
 
+
 def netem(ctx, hostname, interfaces=[], rate='100kbit', loss='0', delay='0'):
-    _netem_reset(ctx, hostname, interfaces)
+    netem_reset(ctx, hostname, interfaces)
     ip_ctrl = ctx.config[hostname]['ip-ctrl']
     for interface in interfaces:
+        # TODO: format elements missing
         cmd = 'sudo tc qdisc add dev {} root netem rate {}'
         cmd = cmd.format(interface, rate)
         out, err = ssh_execute(ctx, hostname, cmd, 10)
         print(out)
         print(err)
 
+
 def prepare_server(ctx):
-	print("Preparing server")
+    print("Preparing server")
+
 
 def prepare_client(ctx):
-	print("Preparing client")
+    print("Preparing client")
+
 
 def analyze_data(ctx):
-	print("Calling matplotlib to analyze msmt results")
+    print("Calling matplotlib to analyze msmt results")
 
 def host_alive(ctx, hostname):
     ''' check if hostname can be called at a host, this checks
     network connectivity and ssh at the same time.
     '''
     cmd = 'hostname'
+    # CHECK functionality
     out, err = ssh_execute(ctx, hostname, cmd)
     print(out, err)
     return True
+
 
 def ssh_execute(ctx, hostname, command, timeout=10, background=False):
     if hostname not in ('alpha', 'beta', 'gamma'):
@@ -46,22 +57,32 @@ def ssh_execute(ctx, hostname, command, timeout=10, background=False):
 
     ssh = None
     key = None
+
     try:
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
         keyfilepath = ctx.config['ssh']['keyfilepath']
         password = ctx.config['ssh']['keypassword']
-        pkey = paramiko.RSAKey.from_private_key_file(keyfilepath, password=password)
+
+        # this creates the private key, assume it is encrypted
+        pkey = paramiko.RSAKey.from_private_key_file(
+            keyfilepath, password=password)
 
         host = ctx.config[hostname]['ip-ctrl']
         port = ctx.config['ssh']['port']
         username = ctx.config['ssh']['username']
-        ssh.connect(host, port, username, None, pkey=key, timeout=1.0)
+
+        # NOTE: the passphrase/password has to be handed over or we cant unlock it
+        # ...still we get an authentication error
+        # ssh.connect(host, int(port), username, password, pkey=pkey, timeout=1.0)
+        # use temporarily user/pwd not certificate
+        ssh.connect(host, int(port), username, yourPw)
 
         if background:
             transport = ssh.get_transport()
             channel = transport.open_session()
+            # what does this
             cmd = '{} > /dev/null 2>&1 &'.format(cmd)
             channel.exec_command(command)
             return None, None
@@ -77,8 +98,8 @@ def ssh_execute(ctx, hostname, command, timeout=10, background=False):
             stdoutstring = stdout.readlines()
             stderrstring = stderr.readlines()
             return stdoutstring, stderrstring
+
     finally:
         if ssh is not None:
             # Close client connection.
             ssh.close()
-
--- tests/throughput_max.py
+++ tests/throughput_max.py
@@ -4,7 +4,10 @@ import shared
 
 def main(ctx):
     print('TEST: {}'.format(os.path.basename(__file__)[:-3]))
+
     # check that beta and gamma are reachable
+    # TODO: check gamma aswell
+    # CHECK functionality
     avail = shared.host_alive(ctx, 'beta')
     if not avail:
         pass
@@ -12,18 +15,19 @@ def main(ctx):
     # reset netem, don't know what was previouly configured
     beta_iface_to_alpha = ctx.config['beta']['netem-interfaces-to-alpha']
     beta_iface_to_gamma = ctx.config['beta']['netem-interfaces-to-gamma']
-    interfaces=[beta_iface_to_alpha, beta_iface_to_gamma]
+    interfaces = [beta_iface_to_alpha, beta_iface_to_gamma]
     shared.netem_reset(ctx, 'beta', interfaces=interfaces)
+
+    # Note in this sceario we dont need to adjust netem to a specific setting
 
     cmd = "kill -9 mapago"
     shared.ssh_execute(ctx, 'gamma', cmd, background=False)
 
     # FIXME: probably $HOME is not expanded, please check!
     cmd = "$HOME/gobin/mapago-server"
+
     shared.ssh_execute(ctx, 'gamma', cmd, background=True)
     # this should run now (please login into gamma, and
     # call ps
 
     # ok, now start local client here. similar to other script
-
-
