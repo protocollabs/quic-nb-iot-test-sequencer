b0356c3a825498d550df4d3310beaf93f2c861b9
Simon Georg Fronhöfer <s.fronhoefer@campus.tu-berlin.de>
2019-09-09 16:51:32 +0200

    Updated: Goodput_delay including the obtained QUIC msmt results
    Signed-off-by: Simon Georg Fronhöfer <s.fronhoefer@campus.tu-berlin.de>

--- tests/goodput_delay.py
+++ tests/goodput_delay.py
@@ -6,35 +6,31 @@ from time import sleep
 import numpy as np
 
 analyzing_rates = [5, 50, 250, 500]
-analyzing_delay = [0, 10, 50, 250, 1000, 10000, 20000]
+analyzing_delay = [0, 10, 50, 250, 1000, 10000]
 yticks_list = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
-
+xticks_list = [0, 250, 1000, 10000]
 
 # copy & paste from evaluation.py
+
+
 def run_test(ctx):
     print('running test: {}'.format(os.path.basename(__file__)[:-3]))
     remoteHosts = ['beta', 'gamma']
     srv_params = {}
     clt_params = {}
- 
+
     supported_protocols = [
-        "quic-throughput",
         "tcp-throughput",
-        "tcp-tls-throughput"
-        # "udp-throughput",
-        ]
- 
-    '''
-    use: start, stop interval or predefined msmt points 
-    start_rate = 10
-    stop_rate = 1000
-    step_rate = 10
-    analyzing_rates = list(range(start_rate, stop_rate + step_rate, step_rate))
-    '''
-    print("rate: ", analyzing_rates)
-    print("delay: ", analyzing_delay)
+        "tcp-tls-throughput",
+        "quic-throughput"
+    ]
 
     num_iterations = 10
+    timeout_ctr_limit = 3
+
+    sim_dur = shared.calc_simulation_time(
+        supported_protocols, num_iterations, timeout_ctr_limit, analyzing_rates, analyzing_delay)
+    print("simulation duration is: {}".format(sim_dur))
 
     iterations = list(range(num_iterations))
 
@@ -55,11 +51,10 @@ def run_test(ctx):
     clt_params['-control-protocol'] = 'tcp'
     clt_params['-streams'] = '1'
     clt_params['-addr'] = '192.186.25.2'
-    clt_params['-deadline'] = '60'
+    clt_params['-deadline'] = '120'
     clt_params['-buffer-length'] = '1400'
     clt_params['-update-interval'] = '1'
 
-    # goodput_rate_avg for all protocols
     total_goodput_rate_avg = {}
     total_results_debug = {}
 
@@ -75,7 +70,7 @@ def run_test(ctx):
         # 2. iterate over rate
         for rate in analyzing_rates:
             print("\n------ configuring rate to: {} --------".format(rate))
-            
+
             # 3. determine bytes for transmission regarding rate
             clt_bytes = int(shared.calc_clt_bytes(rate))
             clt_params['-bytes'] = str(clt_bytes)
@@ -95,54 +90,51 @@ def run_test(ctx):
 
                     # ensures we dont get stuck in a popen.wait(deadline) deadlock
                     timeout_ctr = 0
-                    timeout_ctr_limit = 10
 
                     # reset queue at netem middlebox
                     shared.netem_reset(ctx, 'beta', interfaces=interfaces)
 
                     # 5. we know everything: so configure!
                     shared.netem_configure(
-                    ctx, 'beta', interfaces=interfaces, netem_params={
-                        'rate': '{}kbit'.format(rate), 'delay': '{}'.format(delay)})
-              
-                    # ensure server is running "fresh" per iter => no saved crypto cookies
-                    # note: using this we cant get "ssh" debug data
-                    # due to background cmd
-                    # we could implement a logging routine in mapago writing to a log file on srv...
+                        ctx, 'beta', interfaces=interfaces, netem_params={
+                            'rate': '{}kbit'.format(rate), 'delay': '{}'.format(delay)})
+
                     shared.mapago_reset(ctx, 'gamma')
                     shared.prepare_server(ctx, srv_params)
-                    
+
                     # ensures client mapago creation does not happen before server is ready
                     sleep(5)
 
                     clt_params['-module'] = '{}'.format(protocol)
-                    print("\n starting module: {}".format(clt_params['-module']))
-           
+                    print("\n starting module: {}".format(
+                        clt_params['-module']))
+
                     msmt_results = []
 
                     while len(msmt_results) < 1 and timeout_ctr < timeout_ctr_limit:
                         print("\nIssueing prepare_client!\n")
                         msmt_results = shared.prepare_client(ctx, clt_params)
-                    
+
                         # check if client not terminated
                         if len(msmt_results) < 1:
-                            print("\n!!!!!!Error!!!!!! Client NOT terminated! reissue until client terminates!")
+                            print(
+                                "\n!!!!!!Error!!!!!! Client NOT terminated! reissue until client terminates!")
                             timeout_ctr += 1
 
                     if timeout_ctr >= timeout_ctr_limit:
                         print("\nTimeout ctr limit reached! Iteration failed")
                         kbits_iter = 0
                     else:
-                        kbits_iter = analyze_data(msmt_results, protocol, clt_bytes)
+                        kbits_iter = analyze_data(
+                            msmt_results, protocol, clt_bytes)
 
                     kbits_per_delay.append(kbits_iter)
 
                 kbits_per_delay_normalized = 0
-                
+
                 # account all iters
                 for kbits_iter in kbits_per_delay:
                     kbits_per_delay_normalized += kbits_iter
-
 
                 kbits_per_delay_normalized = kbits_per_delay_normalized / num_iterations
                 print("\n mean kbits per delay: {}".format(
@@ -154,23 +146,25 @@ def run_test(ctx):
                 goodput_rate_quotient_avg = kbits_per_delay_normalized / rate
 
                 # 7. add to list of quietnts for single rate iver losses
-                quotients_single_rate_over_delays.append(goodput_rate_quotient_avg)
+                quotients_single_rate_over_delays.append(
+                    goodput_rate_quotient_avg)
 
                 # 7.5 add los to list
                 analyzed_delay_per_rate.append(delay)
 
             # 8. ok: we got all quoient for a given SINGLE rate and all LOSSES
-            # add it to the list: where we store all RATES and the corresponding list 
+            # add it to the list: where we store all RATES and the corresponding list
             # for the SINGLE rate and all LOSSES
-            quotients_all_rates_over_delays.append(quotients_single_rate_over_delays)
+            quotients_all_rates_over_delays.append(
+                quotients_single_rate_over_delays)
             visited_rate.append(rate)
             visited_delay.append(analyzed_delay_per_rate)
 
-
         # 9. we got the list of lists for a single protocol complete: add it
-        total_goodput_rate_avg[protocol] = (visited_rate, visited_delay, quotients_all_rates_over_delays)
-        shared.save_raw_data(os.path.basename(__file__)[:-3], total_goodput_rate_avg)   
-    
+        total_goodput_rate_avg[protocol] = (
+            visited_rate, visited_delay, quotients_all_rates_over_delays)
+        shared.save_raw_data(os.path.basename(__file__)[
+                             :-3], total_goodput_rate_avg)
 
         print("\n visited_rate: ", visited_rate)
         print("\n visited_delay: ", visited_delay)
@@ -180,7 +174,13 @@ def run_test(ctx):
         sleep(5)
         print("\n next protocol")
 
-    # total_goodput_rate_avg = {"quic-throughput": [[5, 50, 250, 500], [[0, 10, 50, 250, 1000, 10000, 20000], [0, 10, 50, 250, 1000, 10000, 20000], [0, 10, 50, 250, 1000, 10000, 20000], [0, 10, 50, 250, 1000, 10000, 20000]], [[0.08072194827611431, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [0.899948994203095, 0.8627787317193352, 0.9003812061587806, 0.7830943059386358, 0.8535495772270104, 0.0, 0.0], [0.9055167907332836, 0.9019208041901359, 0.8995311273361971, 0.8763481458448378, 0.8558386798968232, 0.0, 0.0], [0.9035125431798576, 0.9058772256592986, 0.9021850534139184, 0.8868459428960387, 0.8429408093912928, 0.0, 0.0]]]}
+    '''
+    QUIC thesis results:
+    - These results were obtained in the context of the measurement
+    - Used this line for verifying the result
+
+    total_goodput_rate_avg = {"tcp-throughput": [[5, 50, 250, 500], [[0, 10, 50, 250, 1000, 10000], [0, 10, 50, 250, 1000, 10000], [0, 10, 50, 250, 1000, 10000], [0, 10, 50, 250, 1000, 10000]], [[0.8987872192411489, 0.7823267454919813, 0.8717731267708974, 0.9449281470690319, 0.7915555942868592, 0.24790445199003258], [0.8914808616112653, 0.9393961996215485, 0.9553501262186983, 0.9014234459920567, 0.8383314888940464, 0.0], [0.9502326564667469, 0.9542850021635289, 0.9561571123219293, 0.9509310693945888, 0.9060625349831072, 0.0], [0.9563853871451465, 0.9563302765309082, 0.9564144806993923, 0.9553549476575104, 0.9240357743805836, 0.0]]], "tcp-tls-throughput": [[5, 50, 250, 500], [[0, 10, 50, 250, 1000, 10000], [0, 10, 50, 250, 1000, 10000], [0, 10, 50, 250, 1000, 10000], [0, 10, 50, 250, 1000, 10000]], [[0.7915266722574326, 0.7601227070334426, 0.7381980505336566, 0.8927837899522644, 0.8842580640813127, 0.07078585190436534], [0.915011005190392, 0.9349899435998139, 0.7538226523850817, 0.7739993436101752, 0.9077166661574337, 0.08717398881425656], [0.8963259250550947, 0.9095146161880139, 0.9074419480214088, 0.8893709701924917, 0.8217028224187859, 0.0], [0.9146203613669235, 0.9309780058311868, 0.8878567577638666, 0.8991129060391729, 0.8414791205156935, 0.0]]], "quic-throughput": [[5, 50, 250, 500], [[0, 10, 50, 250, 1000, 10000], [0, 10, 50, 250, 1000, 10000], [0, 10, 50, 250, 1000, 10000], [0, 10, 50, 250, 1000, 10000]], [[0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [0.4369145107915965, 0.3386327800287026, 0.4508887053335266, 0.22434479680950695, 0.2773192268883254, 0.0], [0.9044825487791093, 0.8881095657532313, 0.901846031337234, 0.8991592469774472, 0.8781436629972122, 0.0], [0.9027135581514798, 0.9059463999240039, 0.9052832465014307, 0.8994225971623447, 0.8782662471078365, 0.0]]]}
+    '''
     plot_data(total_goodput_rate_avg)
 
 
@@ -232,7 +232,7 @@ def analyze_data(msmt_results, protocol, clt_bytes):
         mbits_per_period = (bytes_per_period * 8) / 10**6
         # bytes_rx == # bytes until now received
         bytes_rx = bytes_measurement_point
-        
+
         # this works only if data is send immediately after prev_datetime_max
         # or we pay attention to a period where nothing is transmitted
         prev_datetime_max = datetime_max
@@ -261,8 +261,9 @@ def analyze_data(msmt_results, protocol, clt_bytes):
 
     return Kbits_sec
 
+
 def plot_data(gp_avg):
-    
+
     tcp_gp_avg_first_rate = gp_avg["tcp-throughput"][2][0]
     tcp_gp_avg_second_rate = gp_avg["tcp-throughput"][2][1]
     tcp_gp_avg_third_rate = gp_avg["tcp-throughput"][2][2]
@@ -278,81 +279,157 @@ def plot_data(gp_avg):
     quic_gp_avg_third_rate = gp_avg["quic-throughput"][2][2]
     quic_gp_avg_fourth_rate = gp_avg["quic-throughput"][2][3]
 
-    # one can also use the other protocols loss values. 
-    # also ensures different loss values => add ons for future 
+    # one can also use the other protocols loss values.
+    # also ensures different loss values => add ons for future
     first_rate_delay_values = gp_avg["tcp-throughput"][1][0]
     second_rate_delay_values = gp_avg["tcp-throughput"][1][1]
     third_rate_delay_values = gp_avg["tcp-throughput"][1][2]
     fourth_rate_delay_values = gp_avg["tcp-throughput"][1][3]
 
     fig = plt.figure(figsize=(11, 9))
-    ay1 = plt.subplot(411) 
+    ay1 = plt.subplot(411)
 
-    plt.plot(fourth_rate_delay_values, tcp_gp_avg_fourth_rate, linestyle=':', marker='v', markersize=4, color='#377eb8', label="TCP")
-    plt.plot(fourth_rate_delay_values, tls_gp_avg_fourth_rate, linestyle='-.', marker='^', markersize=4, color='#4daf4a', label="TCPTLS")
-    plt.plot(fourth_rate_delay_values, quic_gp_avg_fourth_rate, linestyle=shared.linestyles['densely dashdotted'], marker='s', markersize=4, color='#ff7f00', label="QUIC")
+    plt.plot(fourth_rate_delay_values, tcp_gp_avg_fourth_rate,
+             linestyle=':', marker='v', markersize=4, color='#377eb8', label="TCP")
+    plt.plot(fourth_rate_delay_values, tls_gp_avg_fourth_rate, linestyle='-.',
+             marker='^', markersize=4, color='#4daf4a', label="TCP/TLS")
+    plt.plot(fourth_rate_delay_values, quic_gp_avg_fourth_rate,
+             linestyle=shared.linestyles['densely dashdotted'], marker='s', markersize=4, color='#ff7f00', label="QUIC")
 
     plt.ylabel('Goodput/rate [%]')
     plt.xlabel('delay [ms]', labelpad=0)
-    plt.xticks(first_rate_delay_values)
+    plt.xticks(xticks_list)
     plt.yticks(yticks_list)
     plt.legend()
     plt.grid(color='darkgray', linestyle=':')
     plt.title('Rate: {} KBit/s'.format(gp_avg["tcp-throughput"][0][3]))
 
-    plt.subplot(412) 
+    ax = plt.gca()
+    ax.set_facecolor('white')
+    plt.setp(ax.spines.values(), color='black')
+    legend = plt.legend()
+    legend_frame = legend.get_frame()
+    legend_frame.set_facecolor('white')
 
-    plt.plot(third_rate_delay_values, tcp_gp_avg_third_rate, linestyle=':', marker='v', markersize=4, color='#377eb8', label="TCP")
-    plt.plot(third_rate_delay_values, tls_gp_avg_third_rate, linestyle='-.', marker='^', markersize=4, color='#4daf4a', label="TCPTLS")
-    plt.plot(third_rate_delay_values, quic_gp_avg_third_rate, linestyle=shared.linestyles['densely dashdotted'], marker='s', markersize=4, color='#ff7f00', label="QUIC")
+    # arrows here
+
+    plt.arrow(250.00, 0.7, 0.0, 0.05, fc="#e41a1c", ec="#e41a1c",
+              head_width=80, head_length=0.1, label="NB-IoT 1")
+    plt.text(0.0, 0.5, 'NB-IoT 1)', fontsize=10, color="#e41a1c")
+
+    plt.arrow(1000.00, 0.3, 0.0, 0.05, fc="#e41a1c", ec="#e41a1c",
+              head_width=80, head_length=0.1, label="NB-IoT 3")
+    plt.text(650.0, 0.2, 'NB-IoT 2)', fontsize=10, color="#e41a1c")
+
+    plt.subplot(412)
+
+    plt.plot(third_rate_delay_values, tcp_gp_avg_third_rate, linestyle=':',
+             marker='v', markersize=4, color='#377eb8', label="TCP")
+    plt.plot(third_rate_delay_values, tls_gp_avg_third_rate, linestyle='-.',
+             marker='^', markersize=4, color='#4daf4a', label="TCP/TLS")
+    plt.plot(third_rate_delay_values, quic_gp_avg_third_rate,
+             linestyle=shared.linestyles['densely dashdotted'], marker='s', markersize=4, color='#ff7f00', label="QUIC")
 
     plt.ylabel('Goodput/rate [%]')
     plt.xlabel('delay [ms]', labelpad=0)
-    plt.xticks(first_rate_delay_values)
+    plt.xticks(xticks_list)
     plt.yticks(yticks_list)
     plt.legend()
     plt.grid(color='darkgray', linestyle=':')
     plt.title('Rate: {} KBit/s'.format(gp_avg["tcp-throughput"][0][2]))
 
+    ax = plt.gca()
+    ax.set_facecolor('white')
+    plt.setp(ax.spines.values(), color='black')
+    legend = plt.legend()
+    legend_frame = legend.get_frame()
+    legend_frame.set_facecolor('white')
 
-    plt.subplot(413) 
+    # arrows here
 
-    plt.plot(second_rate_delay_values, tcp_gp_avg_second_rate, linestyle=':', marker='v', markersize=4, color='#377eb8', label="TCP")
-    plt.plot(second_rate_delay_values, tls_gp_avg_second_rate, linestyle='-.', marker='^', markersize=4, color='#4daf4a', label="TCPTLS")
-    plt.plot(second_rate_delay_values, quic_gp_avg_second_rate, linestyle=shared.linestyles['densely dashdotted'], marker='s', markersize=4, color='#ff7f00', label="QUIC")
+    plt.arrow(250.00, 0.7, 0.0, 0.05, fc="#e41a1c", ec="#e41a1c",
+              head_width=80, head_length=0.1, label="NB-IoT 1")
+    plt.text(0.0, 0.5, 'NB-IoT 1)', fontsize=10, color="#e41a1c")
+
+    plt.arrow(1000.00, 0.3, 0.0, 0.05, fc="#e41a1c", ec="#e41a1c",
+              head_width=80, head_length=0.1, label="NB-IoT 3")
+    plt.text(650.0, 0.2, 'NB-IoT 2)', fontsize=10, color="#e41a1c")
+
+    plt.subplot(413)
+
+    plt.plot(second_rate_delay_values, tcp_gp_avg_second_rate,
+             linestyle=':', marker='v', markersize=4, color='#377eb8', label="TCP")
+    plt.plot(second_rate_delay_values, tls_gp_avg_second_rate, linestyle='-.',
+             marker='^', markersize=4, color='#4daf4a', label="TCP/TLS")
+    plt.plot(second_rate_delay_values, quic_gp_avg_second_rate,
+             linestyle=shared.linestyles['densely dashdotted'], marker='s', markersize=4, color='#ff7f00', label="QUIC")
 
     plt.ylabel('Goodput/rate [%]')
     plt.xlabel('delay [ms]', labelpad=0)
-    plt.xticks(first_rate_delay_values)
+    plt.xticks(xticks_list)
     plt.yticks(yticks_list)
     plt.legend()
     plt.grid(color='darkgray', linestyle=':')
     plt.title('Rate: {} KBit/s'.format(gp_avg["tcp-throughput"][0][1]))
 
+    ax = plt.gca()
+    ax.set_facecolor('white')
+    plt.setp(ax.spines.values(), color='black')
+    legend = plt.legend()
+    legend_frame = legend.get_frame()
+    legend_frame.set_facecolor('white')
 
-    plt.subplot(414) 
+    # arrows here
 
-    plt.plot(first_rate_delay_values, tcp_gp_avg_first_rate, linestyle=':', marker='v', markersize=4, color='#377eb8', label="TCP")
-    plt.plot(first_rate_delay_values, tls_gp_avg_first_rate, linestyle='-.', marker='^', markersize=4, color='#4daf4a', label="TCPTLS")
-    plt.plot(first_rate_delay_values, quic_gp_avg_first_rate, linestyle=shared.linestyles['densely dashdotted'], marker='s', markersize=4, color='#ff7f00', label="QUIC")
+    plt.arrow(250.00, 0.1, 0.0, 0.05, fc="#e41a1c", ec="#e41a1c",
+              head_width=80, head_length=0.1, label="NB-IoT 1")
+    plt.text(0.0, 0.0, 'NB-IoT 1)', fontsize=10, color="#e41a1c")
+
+    plt.arrow(1000.00, 0.1, 0.0, 0.05, fc="#e41a1c", ec="#e41a1c",
+              head_width=80, head_length=0.1, label="NB-IoT 3")
+    plt.text(1000.0, 0.0, 'NB-IoT 2)', fontsize=10, color="#e41a1c")
+
+    plt.subplot(414)
+
+    plt.plot(first_rate_delay_values, tcp_gp_avg_first_rate, linestyle=':',
+             marker='v', markersize=4, color='#377eb8', label="TCP")
+    plt.plot(first_rate_delay_values, tls_gp_avg_first_rate, linestyle='-.',
+             marker='^', markersize=4, color='#4daf4a', label="TCP/TLS")
+    plt.plot(first_rate_delay_values, quic_gp_avg_first_rate,
+             linestyle=shared.linestyles['densely dashdotted'], marker='s', markersize=4, color='#ff7f00', label="QUIC")
 
     plt.ylabel('Goodput/rate [%]')
     plt.xlabel('delay [ms]', labelpad=0)
-    plt.xticks(first_rate_delay_values)
+    plt.xticks(xticks_list)
     plt.yticks(yticks_list)
     plt.legend()
     plt.grid(color='darkgray', linestyle=':')
     plt.title('Rate: {} KBit/s'.format(gp_avg["tcp-throughput"][0][0]))
 
+    ax = plt.gca()
+    ax.set_facecolor('white')
+    plt.setp(ax.spines.values(), color='black')
+    legend = plt.legend()
+    legend_frame = legend.get_frame()
+    legend_frame.set_facecolor('white')
 
-    plt.subplots_adjust(hspace = 0.5)
-   
+    # arrows here
+
+    plt.arrow(250.00, 0.7, 0.0, 0.05, fc="#e41a1c", ec="#e41a1c",
+              head_width=80, head_length=0.1, label="NB-IoT 1")
+    plt.text(0.0, 0.5, 'NB-IoT 1)', fontsize=10, color="#e41a1c")
+
+    plt.arrow(1000.00, 0.3, 0.0, 0.05, fc="#e41a1c", ec="#e41a1c",
+              head_width=80, head_length=0.1, label="NB-IoT 3")
+    plt.text(650.0, 0.2, 'NB-IoT 2)', fontsize=10, color="#e41a1c")
+
+    plt.subplots_adjust(hspace=0.5)
+
     result_file = shared.prepare_result(os.path.basename(__file__)[:-3])
     # fig.suptitle("Measurement campaign: Delay analysis \n {}".format(r'(Rate steps = 4, Delay steps = 6,  Iterations = 4, $t_{deadline} = 60s$)'), fontsize=14)
-    fig.suptitle("Measurement campaign: Delay analysis \n")
+    #fig.suptitle("Impact of delay: Macroscopic analysis\n")
     fig.savefig(result_file, bbox_inches='tight')
 
 
 def main(ctx):
     run_test(ctx)
-    
